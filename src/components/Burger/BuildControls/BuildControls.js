0c98c42873dc086b68469744cf83061eaab854fd
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-27 15:13:44 +0000

    Controller displayed bellow burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Meat', type: 'meat'},
+  {label: 'Cheese', type: 'cheese'},
+];
 
 const buildControls = (props) => (
-  <div>
-    
+  <div className="BuildControls">
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
   </div>
 
 
